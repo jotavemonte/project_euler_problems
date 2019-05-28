ec3f6719c73b4fdf6d74390b06b62654ffa41967
João Victor Monte R. Tavares <jotavemonte@gmail.com>
2019-05-27 23:51:30 -0300

    made problem 2

--- c/problem_2.c
+++ c/problem_2.c
@@ -1,16 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 
 int main() {
     int aux;
     int previous = 1;
     int actual = 2;
+    long sum = actual;
+    bool stopCondition = true;
 
-    for (int i = 2; i < 4E6; i++) {
+    // for (int i = 2; i < 4E6; i++) {
+    //     aux = previous;
+    //     previous = actual;
+    //     actual = previous + aux;
+    //     if (actual % 2 == 0) {
+    //         sum += actual;
+    //     }
+    // }
+
+    while (stopCondition == true) {
         aux = previous;
         previous = actual;
-        actual = previous
-        sum += 
+        actual = previous + aux;
+        if (actual % 2 == 0) {
+            sum += actual;
+        }
+        if (actual > 4E6) {
+            stopCondition = false;
+        }
     }
+    printf("sum: %li", sum);
     return 0;
 }
