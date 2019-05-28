6a442fe9d1085ad8c93b3e9de438e1463bcabca3
João Victor Monte R. Tavares <jotavemonte@gmail.com>
2019-05-27 23:34:49 -0300

    first commit

---
+++ c/problem_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+    int aux;
+    int previous = 1;
+    int actual = 2;
+
+    for (int i = 2; i < 4E6; i++) {
+        aux = previous;
+        previous = actual;
+        actual = previous
+        sum += 
+    }
+    return 0;
+}
